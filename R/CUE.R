9b33e3f7b725d1a88b61d29fbb8a9abe608efbb0
Daniel Pabón <dpabonw@gmail.com>
2019-07-01 15:19:11 +0200

    functions modified, wue documentatation added. aggregation method proggraming added

--- R/CUE.R
+++
@@ -1,9 +0,0 @@
-CUE <- function(GPP, NPP, method = " chambers") {
-  # parameters controls 
-  
-  if (method == "chambers") {
-    cue <-  NPP / GPP
-  }
-  
-  # Apparent seasonal Carbon use efficiency (aCUE)
-}
